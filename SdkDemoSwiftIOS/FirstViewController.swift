b887bd4f5e2eb0c97e25612be376b2f26943d193
David Lin <davidforest@gmail.com>
2020-06-21 22:36:40 +1000

    Initial Commit

---
+++ SdkDemoSwiftIOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SdkDemoSwiftIOS
+//
+//  Created by David Lin on 21/6/20.
+//  Copyright © 2020 Narratiive Audience Measurement. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SdkDemoSwiftIOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SdkDemoSwiftIOS
+//
+//  Created by David Lin on 21/6/20.
+//  Copyright © 2020 Narratiive Audience Measurement. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
